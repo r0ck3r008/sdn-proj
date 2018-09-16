4a68eb1438e5621759ff675ea7b7968dec8b4566
r0ck3r008 <naman.arora@tutanota.com>
2018-09-17 01:45:32 +0530

    made send receive header and source
    Former-commit-id: febb122e60710ed8659572f4914966adb9b9d3ec

---
+++ relay/snd_rcv.c
@@ -0,0 +1,37 @@
+#define NEEDS_CONTROLLER_STRUCT
+
+#include"global_defs.h"
+#include"snd_rcv.h"
+#include"allocate.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+int snd(struct controller *cli, char *cmds, char *reason, char *retval)
+{
+    if(send(cli->sock, cmds, sizeof(char)*512, 0)==-1)
+    {
+        explicit_bzero(retval, sizeof(char)*256);
+        sprintf(retval, "\n[-]Error in sendig %s to %s:%d for %s: %s\n", cmds, inet_ntoa(cli->addr.sin_addr), ntohs(cli->addr.sin_port), reason, strerror(errno));
+        return 1;
+    }
+
+    free(cmds);
+    return 0;
+}
+
+char *rcv(struct controller *cli, char *reason, char *retval)
+{
+    char *cmdr=(char *)allocate("char", 512);
+
+    if(recv(cli->sock, cmdr, sizeof(char)*512, 0)==-1)
+    {
+        explicit_bzero(retval, sizeof(char)*256);
+        sprintf(retval, "\n[-]Error in receving from %s:%d for %s:%s\n", inet_ntoa(cli->addr.sin_addr), ntohs(cli->addr.sin_port), reason, strerror(errno));
+        return NULL;
+    }
+
+    return cmdr;
+}
---
+++ relay/snd_rcv.h
@@ -0,0 +1,7 @@
+#ifndef SND_RCV_H
+#define SND_RCV_H
+
+int snd(struct controller *, char *, char *, char *);
+char *rcv(struct controller *, char *, char *);
+
+#endif
