66ebdb91a23757d1b647fddb8d3c659faf1e048c
r0ck3r008 <naman.arora@tutanota.com>
2018-09-20 12:47:45 +0530

    changed global definitions
    Former-commit-id: a0353e17960be8324bb97a9738d190cc0991e795

--- relay/global_defs.h
+++ relay/global_defs.h
@@ -1,7 +1,14 @@
 #ifndef GLOBAL_DEFS_H
 #define GLOBAL_DEFS_H
 
-    #ifdef NEEDS_CONTROLLER_STRUCT
+int server_sock;
+
+    #ifdef NEEDS_ALL
+        #define NEEDS_STRUCT
+        #define NEEDS_MUTEX
+    #endif
+
+    #ifdef NEEDS_STRUCT
         #include<sys/socket.h>
         #include<netinet/in.h>
         #include<arpa/inet.h>
@@ -10,9 +17,21 @@
             int sock;
             struct sockaddr_in addr;
         } *cli;
+        
+        struct bcast_msg_node
+        {
+            int id;
+            char *msg;
+            struct controller *sender;
+            struct bcast_msg_node *nxt;
+            struct bcast_msg_node *prev;
+        } *bcast_start=NULL;
+
     #endif
 
-
-int server_sock;
+    #ifdef NEEDS_MUTEX
+        #include<pthread.h>
+        pthread_mutex_t speaker=PTHREAD_MUTEX_INITIALIZER;
+    #endif
 
 #endif
