32717e82548def66c1d1e17ffbb11c0298a6f7a7
r0ck3r008 <naman.arora@tutanota.com>
2018-09-21 02:54:23 +0530

    added more allocatable structures
    Former-commit-id: d6a135a35344123cad54cf3b7fb7469017a8021a

--- relay/allocate.c
+++ relay/allocate.c
@@ -2,6 +2,7 @@
 
 #include"global_defs.h"
 #include"allocate.h"
+#include"broadcast.h"
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -26,6 +27,16 @@ void *allocate(char *type, int size)
     {
         ret=malloc(sizeof(struct bcast_msg_node)*size);
         explicit_bzero(ret, sizeof(struct bcast_msg_node)*size);
+    }
+    else if(!strcmp(type, "uint32_t"))
+    {
+        ret=malloc(sizeof(uint32_t)*size);
+        explicit_bzero(ret, sizeof(uint32_t)*size);
+    }
+    else if(!strcmp(type, "struct broadcast_struct"))
+    {
+        ret=malloc(sizeof(struct broadcast_struct)*size);
+        explicit_bzero(ret, sizeof(struct broadcast_struct)*10);
     }
 
     if(ret==NULL)
