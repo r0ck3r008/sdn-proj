7067d02765fbf43ee749e5f0c22e9bbdb5ae8837
r0ck3r008 <naman.arora@tutanota.com>
2018-09-28 22:03:20 +0530

    added sock_create for bcast_socket and server_sock source and header
    Former-commit-id: 5763b3a2b710d6978ef6770915809a85a063e4ef

---
+++ controllers/ryu/apps/relay/sock_create.c
@@ -0,0 +1,54 @@
+#include"sock_create.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<errno.h>
+
+int sock_create(char *argv, int server)
+{
+    int s;
+    struct sockaddr_in addr;
+
+    addr.sin_family=AF_INET;
+    addr.sin_addr.s_addr=inet_addr(strtok(argv, ":"));
+    addr.sin_port=htons((int)strtol(strtok(argv, ":"), NULL, 10));
+    
+    if((s=socket(AF_INET, SOCK_STREAM, 0))==-1)
+    {
+        fprintf(stderr, "\n[-]Error in creating sock: %s", strerror(errno));
+        return -1;
+    }
+
+    if(server)
+    {
+        if(bind(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
+        {
+            fprintf(stderr, "\n[-]Error in binding the server socket: %s\n", strerror(errno));
+            return -1;
+        }
+
+        if(listen(s, 5)==-1)
+        {
+            fprintf(stderr, "\n[-]Error in listning: %s\n", strerror(errno));
+            return -1;
+        }
+
+        printf("\n[!]Server socker bound and listning successfully\n");
+    }
+    else
+    {
+        if(connect(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
+        {
+            fprintf(stderr, "\n[-]Error in connecting to %s:%d: %s\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), strerror(errno));
+            return -1;
+        }
+        printf("\n[!]Connected to %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+    }
+
+    return s;
+}
+
