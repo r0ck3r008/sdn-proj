84cd0d45d4008e2d8eea106652d65b4dcfa71ca0
r0ck3r008 <naman.arora@tutanota.com>
2018-10-10 02:17:42 +0530

    initiated the tcp_child functions
    Former-commit-id: a9da81d7e2fdbd4b647f030e80f09465ff5c7d15

---
+++ relay/tcp_child.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+#include"tcp_child.h"
+#include"snd_rcv.h"
+
+int tcp_child()
+{
+    int stat;
+pthread_t:q
+
+}
---
+++ relay/tcp_child.h
@@ -0,0 +1,6 @@
+#ifndef TCP_CHILD_H
+#define TCP_CHILD_H
+
+int tcp_child();
+
+#endif
