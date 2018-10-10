7064653732b67c50ddbdb3a7252a4626fb2af0ec
r0ck3r008 <naman.arora@tutanota.com>
2018-10-10 22:14:42 +0530

    some fixes in tcp_child, still not working, for compilation
    Former-commit-id: 9ae4768ae3a27da0ae8adbd4c522f7707ad4088d

--- relay/tcp_child.c
+++ relay/tcp_child.c
@@ -1,12 +1,16 @@
+#define NEEDS_STRUCT
+
 #include<stdio.h>
 #include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
+#include"global_defs.h"
 #include"tcp_child.h"
 #include"snd_rcv.h"
 
 int tcp_child()
 {
-    int stat;
-pthread_t:q
-
+    int stat; 
 }
