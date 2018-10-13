3c5e7cab178cd05d0eb5f9e34e7d80fb8707f889
r0ck3r008 <naman.arora@tutanota.com>
2018-10-14 03:52:34 +0530

    initiated tcp and udp connections handelling functions and headers
    Former-commit-id: 9b4f712e610aeed30e85b79ac5793bfb94eb3326

---
+++ controllers/ryu/apps/relay/udp_connector.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<errno.h>
+
+#include"allocate.h"
+#include"udp_connector.h"
---
+++ controllers/ryu/apps/relay/udp_connector.h
@@ -0,0 +1,6 @@
+#ifndef UDP_CONNECTOR
+#define UDP_CONNECTOR
+
+int udp_connector(int);
+
+#endif
