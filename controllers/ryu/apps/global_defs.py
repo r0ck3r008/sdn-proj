1120331cd63eea4d2d3ea4fadf55347e1ea38b3a
r0ck3r008 <naman.arora@tutanota.com>
2018-10-14 03:53:44 +0530

    global definitions for the whole controller
    Former-commit-id: a79fb61aa5200541b773aeb96e7ff4f42103ccce

---
+++ controllers/ryu/apps/global_defs.py
@@ -0,0 +1,8 @@
+blacklist_path='/home/naman/git/sdb-proj/controllers/ryu/apps/blacklist'
+relay_lib_path='/home/naman/git/sdn-proj/controllers/ryu/apps/relay/librelay.so'
+relay_addr='192.168.1.61'
+bind_addr='127.0.0.1'
+udp_sock=0
+tcp_sock=0
+server_sock=0
+relay_lib=None
