c040fc39b115dedf4430a120e565b06ec578a135
r0ck3r008 <naman.arora@tutanota.com>
2018-10-14 04:21:32 +0530

    added sendto udp function in source and header
    Former-commit-id: aa9a4892eb0c5a0e024a222c4745c1c82047bed0

--- controllers/ryu/apps/relay/snd_rcv.c
+++ controllers/ryu/apps/relay/snd_rcv.c
@@ -1,12 +1,15 @@
-#include"snd_rcv.h"
-#include"allocate.h"
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
 #include<unistd.h>
 #include<string.h>
 #include<errno.h>
+
+#include"snd_rcv.h"
+#include"allocate.h"
 
 int snd(int sock, char *cmds, char *reason, char *retval)
 {
@@ -33,4 +36,16 @@ char *rcv(int sock, char *reason, char *retval)
     }
 
     return cmdr;
+}
+
+int snd_to(int sock, struct sockaddr_in *addr, char *cmds, char *reason, char *retval)
+{
+    if(sendto(sock, cmds, sizeof(char)*512, 0, (struct sockaddr *)addr, sizeof(struct sockaddr_in))==-1)
+    {
+        sprintf(retval, "\n[-]Error in sending %s to %s:%d for %s:%s\n", cmds, inet_ntoa(addr->sin_addr), ntohs(addr->sin_port), reason, strerror(errno));
+        return 1;
+    }
+
+    free(cmds);
+    return 0;
 }
--- controllers/ryu/apps/relay/snd_rcv.h
+++ controllers/ryu/apps/relay/snd_rcv.h
@@ -3,5 +3,6 @@
 
 int snd(int, char *, char *, char *);
 char *rcv(int, char *, char *);
+int snd_to(int, struct sockaddr_in *, char *, char *, char *);
 
 #endif
