0aa0e4fa40241fcbf9e4cb9092741e953ec55d09
r0ck3r008 <naman.arora@tutanota.com>
2018-10-14 03:50:45 +0530

    modified sock_create to incorporate udp creation
    Former-commit-id: 38b6678714d86c03d5b2c5fd18799ad6849b4790

--- controllers/ryu/apps/relay/sock_create.c
+++ controllers/ryu/apps/relay/sock_create.c
@@ -8,48 +8,63 @@
 #include<arpa/inet.h>
 #include<errno.h>
 
-int sock_create(char *argv, int server)
+int sock_create(char *argv, int fl)
 {
     int s;
-    struct sockaddr_in addr;
 
-    addr.sin_family=AF_INET;
-    addr.sin_addr.s_addr=inet_addr(strtok(argv, ":"));
-    addr.sin_port=htons((int)strtol(strtok(NULL, ":"), NULL, 10));
-   
-    if((s=socket(AF_INET, SOCK_STREAM, 0))==-1)
+    if(!strcmp(argv, "UDP"))
     {
-        fprintf(stderr, "\n[-]Error in creating sock: %s", strerror(errno));
-        return -1;
-    }
-
-    if(server)
-    {
-        if(bind(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
+        if((s=socket(AF_INET, SOCK_DGRAM, 0))==-1)
         {
-            fprintf(stderr, "\n[-]Error in binding the server socket: %s\n", strerror(errno));
-            return -1;
+            fprintf(stderr, "\n[-]Error in creating udp socket: %s\n", strerror(errno));
         }
-
-        if(listen(s, 5)==-1)
-        {
-            fprintf(stderr, "\n[-]Error in listning: %s\n", strerror(errno));
-            return -1;
-        }
-
-        printf("\n[!]Server socker bound and listning successfully at %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
     }
     else
     {
-        addr.sin_port=htons(12345);
-        if(connect(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
+        struct sockaddr_in addr;
+        addr.sin_family=AF_INET;
+        addr.sin_addr.s_addr=inet_addr(strtok(argv, ":"));
+
+        if((s=socket(AF_INET, SOCK_STREAM, 0))==-1)
         {
-            fprintf(stderr, "\n[-]Error in connecting to %s:%d: %s\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), strerror(errno));
-            return -1;
+            fprintf(stderr, "\n[-]Error in creating tcp socket for %s (flag:%d): %s\n", inet_ntoa(addr.sin_addr), fl, strerror(errno));
         }
-        printf("\n[!]Connected to %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
+        if(fl)//client
+        {
+            addr.sin_port=htons(12346);
+            if(connect(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
+            {
+                fprintf(stderr, "\n[-]Error in connecting to %s:%d: %s\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), strerror(errno));
+                s=-1;
+                goto exit;
+            }
+
+            printf("\n[!]Successfully connected to relay at: %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+        }
+        else
+        {
+            addr.sin_port=htons(12345);
+
+            if(bind(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
+            {
+                fprintf(stderr, "\n[-]Error in binding sock at: %s:%d: %s\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), strerror(errno));
+                s=-1;
+                goto exit;
+            }
+
+            if(listen(s, 5)==-1)
+            {
+                fprintf(stderr, "\n[-]Error in  listning at %s:%d: %s\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), strerror(errno));
+                s=-1;
+                goto exit;
+            }
+
+            printf("\n[!]Tcp server successfully bound and listning\n");
+        }
     }
 
+    exit:
     return s;
 }
 
