229866042b39b407223b5a1e9df7de8b78ac47c0
r0ck3r008 <naman.arora@tutanota.com>
2018-10-14 04:23:46 +0530

    added udp_connector and blacklist functionality as inbuilt along with updation in init_functions
    Former-commit-id: 6662e63de20df24f948b181a857436b9f88afabf

--- controllers/ryu/apps/main.py
+++ controllers/ryu/apps/main.py
@@ -1,8 +1,15 @@
 import ctypes as ct
 import global_defs as gl
 import blacklist as bl
+import tcp_connector as tcp_c
 
 gt.relay_lib=ct.CDLL(gt.relay_lib_path)
+
+def read_blacklist_file():
+    
+    with open(gt.blacklist_path, 'r') as f:
+        gt.blacklist=f.read().strip().split('\n')
+
 
 def init_all_functions():
     #sock_create
@@ -10,7 +17,8 @@ def init_all_functions():
     gt.relay_lib.sock_create.argtypes=[ct.c_char_p, ct.c_int]
 
     #client_run
-    gt.relay_lib.client_run.argtypes=[ct.c_int]
+    gt.relay_lib.udp_connector.restype=ct.c_int
+    gt.relay_lib.udp_connector.argtypes=[ct.c_int, ct.c_char_p]
 
 
 def sock_create():
@@ -30,8 +38,15 @@ def sock_create():
     if gt.server_sock==-1:
         print('Error in creating and binding sock at {}'.format(gt.server_addr))
 
+def connect_udp():
+    argv=gt.relay_addr+'12345'
+    if gt.relay_lib.udp_connector(ct.c_int(gt.udp_sock), ct.c_char_p(addr.encode()))==-1:
+        print('[-]Error in sending udp rsvp to {}'.format(addr))
+
 def init_script():
     bl.read_blacklist_file()
     init_all_functions()
     sock_create()
+    connect_udp()
+    tcp_c.connect_tcp()
 
