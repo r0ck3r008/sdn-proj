9beb9737add640a05eeaac4aa977cc52ada54c8f
r0ck3r008 <naman.arora@tutanota.com>
2018-11-08 14:43:27 +0530

    added explanations to global definitions
    Former-commit-id: caf5fdcfd6dc1c5e16251b7edcfbea8995dc2b04

--- controllers/ryu/apps/global_defs.py
+++ controllers/ryu/apps/global_defs.py
@@ -1,16 +1,29 @@
+#absolute path of some important files
 blacklist_path='/home/naman/git/sdb-proj/controllers/ryu/apps/blacklist'
 relay_lib_path='/home/naman/git/sdn-proj/controllers/ryu/apps/relay/librelay.so'
+
+#ip addresses of the realy and to be bound
 relay_addr='192.168.1.61'
 bind_addr='127.0.0.1'
+
+#main host database
 hosts={}
+
+#all the sockets
 udp_sock=0
 tcp_sock=0
 server_sock=0
 sock=0
+
+#c library import wrapper
 relay_lib=None
+
+#pipes for the multiprocesses
 p1=None
 p2=None
 c1=None
 c2=None
+
+#bounded semaphores
 reader=None
 writer=None
