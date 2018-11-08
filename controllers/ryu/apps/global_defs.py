8258bc339758d825c7f7686e7c360a97fc90c720
r0ck3r008 <naman.arora@tutanota.com>
2018-11-08 23:57:23 +0530

    made all definitions under comments explaining them, added blacklist[] global list, make blacklist_path and relay_path relative to docker container of ryu
    Former-commit-id: c7aa7bd2fe467fe2931b4873ce7eeb8302084c36

--- controllers/ryu/apps/global_defs.py
+++ controllers/ryu/apps/global_defs.py
@@ -1,13 +1,14 @@
 #absolute path of some important files
-blacklist_path='/home/naman/git/sdb-proj/controllers/ryu/apps/blacklist'
-relay_lib_path='/home/naman/git/sdn-proj/controllers/ryu/apps/relay/librelay.so'
+blacklist_path='/ryu/apps/blacklist'
+relay_lib_path='/ryu/apps/relay/librelay.so'
 
 #ip addresses of the realy and to be bound
 relay_addr='192.168.1.61'
 bind_addr='127.0.0.1'
 
-#main host database
+#main host and blacklist database
 hosts={}
+blacklist=[]
 
 #all the sockets
 udp_sock=0
