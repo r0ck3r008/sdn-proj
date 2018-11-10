cb76689710ee4f358f29e65042108aaedc5d56ca
r0ck3r008 <naman.arora@tutanota.com>
2018-11-10 21:09:42 +0530

    removed the need to receive 2 arguments in server_workings along with shifting of alarm to saperate module. Also tcp_child is no longer a subprocess due to multiple creation of tcp_sock with forking
    Former-commit-id: 70f527391976bc8818f9258cf5b56c5f2ea4da5b

--- relay/server.c
+++ relay/server.c
@@ -13,47 +13,33 @@
 #include"global_defs.h"
 #include"udp_child.h"
 #include"tcp_child.h"
+#include"alarm.h"
 #include"sock_create.h"
 #include"server.h"
 
 
-struct alarm_st
-{
-    int time;
-    int *a;
-};
-
-int server_workings(char *argv1, char *argv2)
+int server_workings(char *argv)
 {
     pthread_t alarm_th;
     int stat, a=1;
     struct alarm_st a_s;
     a_s.time=5;
     a_s.a=&a;
-    pid_t child_pid;
+    pid_t child_pid=fork();
 
-    //create sockets
-    if((udp_sock=sock_create(argv1, 3))==-1)
-    {
-        return 1;
-    }
-    if((tcp_sock=sock_create(argv2, 1))==-1)
-    {
-        return 1;
-    }
-
-    if((child_pid=fork())!=0)
+    if(child_pid!=0)
     {
         //parent
+        //handle udp sub-process
         for(int i=0; a; i++)
         {
             if(!i)
             {
-                if((stat=pthread_create(&alarm_th, NULL, alarm_run, &a_s))==0)
-                {
-                    fprintf(stderr, "\n[-]Error in setting alarm: %s\n", strerror(stat));
-                    _exit(-1);
-                }
+               if(set_alarm(&a_s))
+               {
+                   fprintf(stderr, "\n[-]Error in setting the alarm\n");
+                   break;
+               } 
             }
         }
         if((stat=kill(child_pid, SIGTERM))!=0)
@@ -61,31 +47,26 @@ int server_workings(char *argv1, char *argv2)
             fprintf(stderr, "\n[-]Error in killing udp server: %s\n", strerror(stat));
             _exit(-1);
         }
-    }
-    else
-    {
-        //child-> udp_server
-        if(udp_child())
+        close(udp_sock);
+
+        //handle tcp server now
+        if((tcp_sock=sock_create(argv, 1))==-1)
         {
             return 1;
         }
-    }
-    
-    if(fork()==0)
-    {
         if(tcp_child())
         {
             return 1;
         }
     }
+    else
+    {
+        //create socket
+        if((udp_sock=sock_create(argv, 3))==-1)
+        {
+            return 1;
+        }
+        //child-> udp_server
+        udp_child();
+    }
 }
-
-void *alarm_run(void *alarm)
-{
-    struct alarm_st *a_s=(struct alarm_st *)alarm;
-
-    sleep(a_s->time);
-
-    pthread_exit(NULL);
-}
-
--- relay/server.h
+++ relay/server.h
@@ -1,7 +1,6 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-int server_workings(char *, char *);
-void *alarm_run(void *);
+int server_workings(char *);
 
 #endif
