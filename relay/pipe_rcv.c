7f556525c19f928059ccd9c983a79698df4ebd88
r0ck3r008 <naman.arora@tutanota.com>
2018-11-29 03:50:35 +0530

    added sys/socket.h, netinet/in.h and arpa/inet.h for addr definition
    Former-commit-id: f051ca475236ccd660a7d8d04c3f8dcdf751b9c3

--- relay/pipe_rcv.c
+++ relay/pipe_rcv.c
@@ -6,6 +6,9 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 #include<unistd.h>
 #include<pthread.h>
 #include<errno.h>
