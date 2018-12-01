9e5e248026bad2da02b0b7281a78a50d55fbb714
r0ck3r008 <naman.arora@tutanota.com>
2018-12-01 15:50:42 +0530

    rewritten sock_create to elemeinate udp_sock being created
    Former-commit-id: f1f1f820267c4305beafc2ad37d9f4e538aa0fdf

--- relay/sock_create.c
+++ relay/sock_create.c
@@ -1,5 +1,3 @@
-#include"sock_create.h"
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -8,82 +6,47 @@
 #include<arpa/inet.h>
 #include<errno.h>
 
-//weight of udp is 2 server is 1, ie: 0 is tcp-client, 1 is tcp server, 2 is usp client, 3 is udp server
-int sock_create(char *argv, int udp_server)
+#include"sock_create.h"
+
+int sock_create(char *addr, int flag)
 {
-    int s, ret=0;
-    struct sockaddr_in addr;
-    addr.sin_family=AF_INET;
-    addr.sin_addr.s_addr=inet_addr(argv);
-    
-    switch(udp_server)
+    int s;
+    struct sockaddr_in addr_st;
+    explicit_bzero(&addr_st, sizeof(struct sockaddr_in));
+    addr_st.sin_family=AF_INET;
+    addr_st.sin_addr.s_addr=inet_addr(addr);
+
+    if((s=socket(AF_INET, SOCK_STREAM, 0))==-1)
     {
-     case 0://tcp client
-         if((s=socket(AF_INET, SOCK_STREAM, 0))==-1)
-         {
-             fprintf(stderr, "\n[-]Error in creating tcp-client socket: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         addr.sin_port=htons(6666);
-         if(connect(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
-         {
-             fprintf(stderr, "\n[-]Error in connecting tcp-client socket: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         printf("\n[!]Connection established to %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
-         break;
-
-     case 1://tcp-server
-         if((s=socket(AF_INET, SOCK_STREAM, 0))==-1)
-         {
-             fprintf(stderr, "\n[-]Error in creating tcp-server sock: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         addr.sin_port=htons(12346);
-         if(bind(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
-         {
-             fprintf(stderr, "\n[-]Error in binding for tcp-server: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         if(listen(s, 5)==-1)
-         {
-             fprintf(stderr, "\n[-]Error in listning to tcp-server: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         printf("\n[!]Tcp-server bound and listning!\n");
-         break;
-
-     case 3://udp server
-         if((s=socket(AF_INET, SOCK_DGRAM, 0))==-1)
-         {
-             fprintf(stderr, "\n[-]Error in creating udp server sock: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         addr.sin_port=htons(12345);
-         if(bind(s, (struct sockaddr *)&addr, sizeof(addr))==-1)
-         {
-             fprintf(stderr, "\n[-]Error in binding udp-server socket: %s\n", strerror(errno));
-             ret=-1;
-             break;
-         }
-         printf("\n[!]Udp-server socket bound successfully\n");
-         break;
-
-     default:
-         fprintf(stderr, "\n[!]Invalid args!!\n");
-         break;
+        fprintf(stderr, "\n[-]Error in creating socket for addr %s: %s\n", addr, strerror(errno));
+        goto exit;
     }
 
-    if(!ret)
+    if(flag)//server
     {
-        return s;
+        addr_st.sin_port=htons(12345);
+        if(bind(s, (struct sockaddr *)&addr_st, sizeof(addr_st))==-1)
+        {
+            fprintf(stderr, "\n[-]Error in binding address %s: %s\n", addr, strerror(errno));
+            s=-1;
+            goto exit;
+        }
+        if(listen(s, 5)==-1)
+        {
+            fprintf(stderr, "\n[-]Error in listning on %s: %s\n", addr, strerror(errno));
+            s=-1;
+        }
     }
-    return ret;
+    else
+    {
+        addr_st.sin_port=htons(6666);
+        if(connect(s, (struct sockaddr *)&addr_st, sizeof(addr_st))==-1)
+        {
+            fprintf(stderr, "\n[-]Error in connecting to %s at 6666: %s\n", addr, strerror(errno));
+            s=-1;
+        }
+    }
+
+exit:
+    return s;
 }
-
