2d483bbc6b384c888cf1af2ef830f1a9b090acaa
r0ck3r008 <naman.arora@tutanota.com>
2018-12-18 01:45:36 +0530

    divided _general_equate to sb functions, renamed internally called modules by appending underscore and moved equating start to tmp to its respective function
    Former-commit-id: 28a826ec5711395c28667d5f137edc94d40abd58

--- relay/list.c
+++ relay/list.c
@@ -3,6 +3,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<sys/un.h>
 
 #include"global_defs.h"
 #include"list.h"
@@ -11,14 +15,13 @@
 void add_node(union node *new, union node *start, int flag)//0 for ctrlr, 1 for bmn
 {
     union node *tmp;
-    static union node *curr[2];//0 for ctrlr, 1 for bmn
+    static union node *curr[3];//0 for ctrlr, 1 for bmn
     
-    general_equate(tmp, new, (4+flag));
+    _general_equate(tmp, new, (4+flag));
 
     if(start==NULL)
     {
-        general_equate(start, tmp, 2);
-        start=tmp;
+        _general_equate(start, tmp, 2);
         start->nxt=tmp;
         tmp->prev=start;
     }
@@ -31,41 +34,65 @@ void add_node(union node *new, union node *start, int flag)//0 for ctrlr, 1 for 
     curr[flag]=tmp;
 }
 
-void general_equate(union node *a, union node *b, int flag)//0 for start, 1 for tmp
+void _general_equate(union node *a, union node *b, int flag)//0 for start, 1 for tmp
 {
     a=(union node *)allocate("union node", 1);
 
     switch(flag)
     {
      case 2:
-        a->prev=NULL;
-        a->nxt=NULL;
-        a->tag=-1;
-        a->ctrlr=NULL;
-        a->bmn=NULL;
+        _general_equate_start(a, b);
         break;
 
      case (4+0)://tmp for ctrlr
-        a->tag=b->tag;
-        a->ctrlr=(struct controller *)allocate("struct controller", 1);
-        a->ctrlr->id=a->tag;
-        a->ctrlr->bcast_sock=b->ctrlr->bcast_sock;
-        a->ctrlr->sock=b->ctrlr->sock;
-        memcpy(a->ctrlr->addr, b->ctrlr->addr, sizeof(b->ctrlr->addr));
+        _equate_ctrlr(a, b);
         break;
 
      case (4+1)://tmp for bmn
-        a->tag=b->tag;
-        a->bmn=(struct bcast_msg_node *)allocate("struct bcast_msg_node", 1);
-        a->bmn->id=a->tag;
-        a->bmn->done=0;
-        //msg
-        a->bmn->msg=(char *)allocate("char", 512);
-        sprintf(a->bmn->msg, "%s", b->bmn->msg);
-        //controller
-        a->bmn->sender=b->bmn->sender;
+        _equate_bmn(a, b);
         break;
+
+     case (4+2)://tmp for local struct
+        _equate_local(a, b);
     }
+}
+
+void _general_equate_start(union node *a, union node *b)
+{
+    a->prev=NULL;
+    a->nxt=NULL;
+    a->tag=-1;
+    a->ctrlr=NULL;
+    a->bmn=NULL;
+    a=b;
+}
+
+void _equate_ctrlr(union node *a, union node *b)
+{
+    a->tag=b->tag;
+    a->ctrlr=(struct controller *)allocate("struct controller", 1);
+    a->ctrlr->bcast_sock=b->ctrlr->bcast_sock;
+    a->ctrlr->sock=b->ctrlr->sock;
+    memcpy(&a->ctrlr->addr, &b->ctrlr->addr, sizeof(struct sockaddr_in));
+}
+
+void _equate_bmn(union node *a, union node *b)
+{
+    a->tag=b->tag;
+    a->bmn=(struct bcast_msg_node *)allocate("struct bcast_msg_node", 1);
+    a->bmn->done=0;
+    //msg
+    a->bmn->msg=(char *)allocate("char", 512);
+    sprintf(a->bmn->msg, "%s", b->bmn->msg);
+    //controller
+    a->bmn->sender=b->bmn->sender;
+}
+
+void _equate_local(union node *a, union node *b)
+{
+    a->tag=b->tag;
+    a->local->sock=b->local->sock;
+    memcpy(&a->local->addr, &b->local->addr, sizeof(struct sockaddr_un));
 }
 
 union node *find_node(union node *start, int tag)
--- relay/list.h
+++ relay/list.h
@@ -2,7 +2,11 @@
 #define LIST_H
 
 void add_node(union node *, union node *, int);
-void general_equate(union node *, union node *, int);
+void _general_equate(union node *, union node *, int);
+void _general_equate_start(union node *, union node *);
+void _equate_ctrlr(union node *, union node *);
+void _equate_bmn(union node *, union node *);
+void _equate_local(union node *, union node *);
 union node *find_node(union node *, int);
 int del_node(union node *, int, int);
 int list_len(union node *);
