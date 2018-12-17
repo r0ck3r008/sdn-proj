9385e700716a77f6821b045743f40cb1a91fb6fc
r0ck3r008 <naman.arora@tutanota.com>
2018-12-18 01:51:04 +0530

    removed NEEDS_ALL and replaced with NEEDS_STRUCT, added new fork in main parent process for mem_mgr UDS
    Former-commit-id: 58294fce816e7d9323ef9a48a00c158d5fb64cae

--- relay/server.c
+++ relay/server.c
@@ -9,18 +9,34 @@
 #include"server.h"
 #include"sock_create.h"
 #include"tcp_child.h"
+#include"mem_mgr.h"
 
 int server_workings(char *argv)
 {
 //TODO: add more parent server functionality like live monitoring or redundancy;
-    if((tcp_sock=sock_create(argv, 1))==-1)
-    {
-        return 1;
-    }
-
+    pid_t main_pid=getpid();
+    
     if(fork()==0)
     {
-        //child
-        tcp_child();
+        //mem manager
+        if(mem_mgr())
+        {
+            _exit(-1);
+        }
+        
+    }
+
+    if(getpid()==main_pid)
+    {
+        if(fork()==0)
+        {
+            if((tcp_sock=sock_create(argv, 1))==-1)
+            {
+                return 1;
+            }
+
+            //child
+            tcp_child();
+        }
     } 
 }
