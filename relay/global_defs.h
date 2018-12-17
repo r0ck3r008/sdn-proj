475ea46e3a3945bd2e5e1a2da0f2d8a77d6a913e
r0ck3r008 <naman.arora@tutanota.com>
2018-12-18 01:44:16 +0530

    removed mutex, NEEDS_MUTEX, and NEEDS_ALL as it is now a forking server
    Former-commit-id: 88f4022bc71c7bda0f6b6547f1a034bb2436eae4

--- relay/global_defs.h
+++ relay/global_defs.h
@@ -2,46 +2,38 @@
 #define GLOBAL_DEFS_H
 
 int tcp_sock;
-int cli_num;
 int *done_bcast_nodes;
-
-    #ifdef NEEDS_ALL
-        #define NEEDS_STRUCT
-        #define NEEDS_MUTEX
-    #endif
 
     #ifdef NEEDS_STRUCT
 
         struct controller
         {
-            int id;
             int bcast_sock, sock;
             struct sockaddr_in addr;
         };
         
         struct bcast_msg_node
         {
-            int id;
             int done;
             char *msg;
             struct controller *sender;
         };
+
+        struct local_struct
+        {
+            int sock;
+            struct sockaddr_un addr;
+        }
 
         union node
         {
             int tag;
             struct controller *ctrlr;
             struct bcast_msg_node *bmn;
+            struct local_struct *local;
             union node *nxt;
             union node *prev;
-        } *ctrlr_start, *bmn_start;
+        } *ctrlr_start, *bmn_start, *local_start;
 
     #endif
-
-    #ifdef NEEDS_MUTEX
-
-        pthread_mutex_t speaker=PTHREAD_MUTEX_INITIALIZER;
-
-    #endif
-
 #endif
