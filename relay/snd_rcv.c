175b94fb2fc3a354962e1b65d0bb8cd065a66db2
r0ck3r008 <naman.arora@tutanota.com>
2018-12-23 01:37:49 +0530

    removed read/write functions from snd/rcv
    Former-commit-id: 5aaa427a84420b15a7644fce36c06f8708b57189

--- relay/snd_rcv.c
+++ relay/snd_rcv.c
@@ -7,48 +7,26 @@
 #include"snd_rcv.h"
 #include"allocate.h"
 
-int snd(int fd, char *cmds, char *reason, int flag)
+int snd(int fd, char *cmds, char *reason)
 {
-    if(flag)
+    if(send(fd, cmds, sizeof(char)*512, 0)==-1)
     {
-        if(send(fd, cmds, sizeof(char)*512, 0)==-1)
-        {
-            fprintf(stderr, "\n[-]Error in sendig %s for %s: %s\n", cmds, reason, strerror(errno));
-            return 1;
-        }
-    }
-    else
-    {
-        if(write(fd, cmds, sizeof(char)*512)==-1)
-        {
-            fprintf(stderr, "\n[-]Error in writing %s for %s: %s\n", cmds, reason, strerror(errno));
-            return 1;
-        }
+        fprintf(stderr, "\n[-]Error in sendig %s for %s: %s\n", cmds, reason, strerror(errno));
+        return 1;
     }
     
     deallocate(cmds, "char", 512);
     return 0;
 }
 
-char *rcv(int fd, char *reason, int flag)
+char *rcv(int fd, char *reason)
 {
     char *cmdr=(char *)allocate("char", 512);
 
-    if(flag)
+    if(recv(fd, cmdr, sizeof(char)*512, 0)==-1)
     {
-        if(recv(fd, cmdr, sizeof(char)*512, 0)==-1)
-        {
-            fprintf(stderr, "\n[-]Error in receving from for %s:%s\n", reason, strerror(errno));
-            return NULL;
-        }
-    }
-    else
-    {
-        if(read(fd, cmdr, sizeof(char)*512)==-1)
-        {
-            fprintf(stderr, "\n[-]Error in reading for %s: %s\n", reason, strerror(errno));
-            return NULL;
-        }
+        fprintf(stderr, "\n[-]Error in receving for %s: %s\n", reason, strerror(errno));
+        return NULL;
     }
 
     return cmdr;
--- relay/snd_rcv.h
+++ relay/snd_rcv.h
@@ -1,7 +1,7 @@
 #ifndef SND_RCV_H
 #define SND_RCV_H
 
-int snd(int, char *, char *, int);
-char *rcv(int, char *, int);
+int snd(int, char *, char *);
+char *rcv(int, char *);
 
 #endif
