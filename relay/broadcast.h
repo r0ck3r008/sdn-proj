18abec4bd10bf29f94df42d69c384dbc9761b65e
r0ck3r008 <naman.arora@tutanota.com>
2018-12-26 13:55:49 +0530

    expected prototype of send_pkt_back is same as broadcast
    Former-commit-id: f5fd6eac164ce21e703667f2f94b12dc1326d0e1

--- relay/broadcast.h
+++ relay/broadcast.h
@@ -4,6 +4,6 @@
 int broadcast(struct controller *, char *);
 void broadcast_run(union node *, char *);
 void *cleanup_run(void *);
-int send_pkt_back(struct controller *, int, char *);
+int send_pkt_back(struct controller *, char *);
 
 #endif
