b91540803504cef54b2c7ee0b0c0bbefb82a10e4
r0ck3r008 <naman.arora@tutanota.com>
2018-12-27 00:10:56 +0530

    added broadcast function except bmn feature
    Former-commit-id: 8987cd7845d40488d704c51c8c55005ee700ade9

--- relay/broadcast.c
+++ relay/broadcast.c
@@ -12,43 +12,79 @@
 #include<errno.h>
 
 #include"global_defs.h"
+#include"lock_and_exec.h"
 #include"broadcast.h"
 #include"list.h"
 #include"allocate.h"
 #include"snd_rcv.h"
 
-int broadcast(struct controller *sender, char *cmds, pthread_mutex_t *bmn_mtx)
+int broadcast(struct controller *sender, char *cmds, struct mutex_call *mcall)
 {
+    struct mutex_call *mcall2=alloc_mcall(0, 2, mcall->ctrlr_ro, mcall->ctrlr);
+    struct func_call *fcall=alloc_fcall(3);
+    int list_len=0, ret=0;
     pid_t main_pid=getpid();
-    int num_ctrlr=list_len(ctrlr_start);
 
-    if(num_ctrlr==-1)
+    if(lock_and_exec(mcall2, fcall, 2, ctrlr_start, &list_len))
     {
-        fprintf(stderr, "\n[-]Empty list passed to bcast function for controller %s for cmd: %s\n", inet_ntoa(sender->addr.sin_addr), cmds);
-        return -1;
+        fprintf(stderr, "\n[-]Error in list_len for %s\n", inet_ntoa(sender->addr.sin_addr));
+        ret=1;
+        goto exit;
     }
 
-    {//limiting the curr to scope of for loop only
-        union node *curr=ctrlr_start->nxt;
-        for(int i=0; i<num_ctrlr || curr->nxt!=NULL; i++, curr=curr->nxt)
+    if(_lock(mcall2))
+    {
+        fprintf(stderr, "\n[-]Lock failed for %s", inet_ntoa(sender->addr.sin_addr));
+        ret=1;
+        goto exit;
+    }
+
+    union node *curr=ctrlr_start->nxt;
+    struct controller *recepient;
+    for(int i=0; i<list_len; i++)
+    {
+        if(curr->ctrlr!=sender)
         {
-            char *addr=inet_ntoa(curr->ctrlr->addr.sin_addr);
-            if(strcmp(addr, inet_ntoa(sender->addr.sin_addr))) //same controller skip
+            //broadcast
+            if(fork()==0)
             {
-                int tag=curr->tag;
-                if(fork()==0)
-                {
-                    //child
-                    int tag_for_child=tag;
-                    broadcast_run(find_node(ctrlr_start, tag_for_child), cmds);
-                    break;
-                }
+                recepient=curr->ctrlr;
+                _broadcast_run(recepient, cmds);
+                goto exit;
             }
         }
+        curr=curr->nxt;
+    }
+
+    if(_unlock(mcall2))
+    {
+        fprintf(stderr, "\n[-]Unlock failed for %s", inet_ntoa(sender->addr.sin_addr));
+        ret=1;
+        goto exit;
+    }
+
+exit:
+    if(getpid()==main_pid)
+    {
+        deallocate(mcall2, "struct mutex_call", 1);
+        deallocate(fcall, "struct func_call", 1);
+        return ret;   
     }
 }
 
-void broadcast_run(union node *recepient, char *cmds)
+void _broadcast_run(struct controller *recepient, char *cmds)
 {
-    
+    if(snd(recepient->bcast_sock, cmds, "send broadcast msg", 0))
+    {
+        fprintf(stderr, "\n[-]Error in sending bcast %s to %s\n", cmds, inet_ntoa(recepient->addr.sin_addr));
+    }
+}
+
+void *cleanup_run(void *a)
+{
+
+}
+
+int send_pkt_back(struct controller *recepient, char *cmds, struct mutex_call *mcall)
+{
 }
--- relay/broadcast.h
+++ relay/broadcast.h
@@ -1,9 +1,9 @@
 #ifndef BROADCAST_H
 #define BROADCAST_H
 
-int broadcast(struct controller *, char *, pthread_mutex_t *);
-void broadcast_run(union node *, char *);
+int broadcast(struct controller *, char *, struct mutex_call *);
+void _broadcast_run(struct controller *, char *);
 void *cleanup_run(void *);
-int send_pkt_back(struct controller *, char *, pthread_mutex_t *);
+int send_pkt_back(struct controller *, char *, struct mutex_call *);
 
 #endif
