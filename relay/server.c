abe39310250f52161c25b4b4f1bdfcff181766d2
r0ck3r008 <naman.arora@tutanota.com>
2018-12-28 01:59:57 +0530

    defined NEEDS_STRUCT and added sys/socket.h, netinet/in.h and arpa/inet.h so as to resolve undefined conflicts
    Former-commit-id: 2125d6b0ef0fe7d7e555eb40fee602d1678644d7

--- relay/server.c
+++ relay/server.c
@@ -1,7 +1,12 @@
+#define NEEDS_STRUCT
+
 #include<stdio.h>
 #include<string.h>
 #include<sys/wait.h>
 #include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 #include<unistd.h>
 #include<errno.h>
 
