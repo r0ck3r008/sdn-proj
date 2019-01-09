002f9381697c739f8ea4b1f039aab6a75e714729
r0ck3r008 <naman.arora@tutanota.com>
2019-01-10 00:13:33 +0530

    added the update_db function and get_self_ip functions to update existance in db
    Former-commit-id: 58f2d38bd596d17df10187cec90e90379e650efe

--- controllers/ryu/apps/main.py
+++ controllers/ryu/apps/main.py
@@ -8,23 +8,49 @@ def sock_create(addr, flag):
         s=socket(AF_INET, SOCK_STREAM)
         print('[!]Socket created')
 
-        if(flag):#this is a client
+        if flag==1:#this is a client or a db_interface or to get self ip
             s.connect(addr)
             print('[!]Successfully connected to {}:{}'.format(addr[0], addr[1]))
-        else:#this is a server
+        elif flag==0:#this is a server
             s.bind(addr)
             s.listen(5)
             print('[!]Successfully bound and listning on {}:{}'.format(addr[0], addr[1]))
+
+        return s
     except Exception as e:
         print('[-]Error in sock_create: {}'.format(e))
 
 
+def get_self_addr():
+    #connect(fake)
+    s=sock_create(('1.1.1.1', 80))
+    #get name and disconnect
+    ret=s.getsockname()[0]
+    s.close()
+
+    return ret
+
+def update_db(self_ip):
+    #connect
+    db_sock=sock_create(gdef.db_interface_addr)
+    
+    query=("INSERT INTO controllers VALUES ('{}')".format(self_ip))
+    db_sock.send(query.encode())
+
+    print('[!]Received from db_interface: {}'.format(db_sock.recv().decode()))
+
+    db_sock.close()
+
 def init_main():
+    self_ip=get_self_ip()
+    #database handller
+    update_db(self_ip)
+        
     #connect to relay
-    gdef.bcast_sock=sock_create(gdef.relay_addr, flag=1)
+#    gdef.bcast_sock=sock_create((self_ip, 6666), flag=1)
 
     #get a connection back from relay
-    sock=sock_create(gdef.self_addr, flag=0)
-    gdef.sock=sock.accept()
-    print('[!]Got relay connection back')
+#    sock=sock_create(gdef.self_addr, flag=0)
+#    gdef.sock=sock.accept()
+#    print('[!]Got relay connection back')
 
