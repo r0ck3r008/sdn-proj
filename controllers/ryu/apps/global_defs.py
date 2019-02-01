786ae8560cd3cb4cbdf7acd2c078df93ab91f59d
r0ck3r008 <naman.arora@tutanota.com>
2019-02-01 23:17:51 +0530

    changed global ip definitions
    Former-commit-id: fa3bd6a422ce2e4fefe90cdc76fb2b56322ffd9a

--- controllers/ryu/apps/global_defs.py
+++ controllers/ryu/apps/global_defs.py
@@ -1,7 +1,3 @@
-#the two sockets, initialized to 0
-bcast_sock=0
-sock=0
-
 #addresses
-relay_addr=('192.168.225.57', 12345)
-db_interface_addr=('192.168.225.51', 12346)
+relay_addr=('10.3.53.10', 12345)
+db_interface_addr=('10.3.53.10', 12346)
