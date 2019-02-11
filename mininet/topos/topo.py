2a5d7beeabc22fe02d3a158f954cd62ffff3e291
r0ck3r <naman.arora@tutanota.com>
2019-02-12 00:28:46 +0530

    datacenter topology
    Former-commit-id: 8c6807d491bcabcaf2613208b2d25766ac32317e

--- mininet/topos/topo.py
+++ mininet/topos/topo.py
@@ -1,51 +1,50 @@
-from argparse import ArgumentParser
-from mininet.net import Mininet
-from mininet.node import RemoteController as rc
-from mininet.cli import CLI as cli
-from libnacl import randombytes_uniform
+from mininet.topo import Topo
 
-def choice(t):
-    ret=t[randombytes_uniform(len(t))]
-    t.remove(ret)
-    return ret
+class dcenterTopo(Topo):
+    def build(self, n_racs, n_hosts):
+        switches=self.init_switches(n_racs)
+        hosts=self.init_hosts(n_racs, n_hosts)
+        topo=self.link_up(switches, hosts)
 
-if __name__=='__main__':
-    parser=ArgumentParser()
-    parser.add_argument('-c', '--controller', required=True, metavar='', dest='ctrlr_ip', help='The IP of the controller')
-    parser.add_argument('-H', '--hosts', required=True, type=int, metavar='', dest='hosts', help='The number of hosts to be added')
-    parser.add_argument('-s', '--switches', required=True, type=int, metavar='', dest='swi', help='The number of switches to be added')
-    args=parser.parse_args()
+    def init_switches(self, n_racs):
+        switches=[]
+        root=self.addSwitch('s1')
+        switches.append(root)
 
-    print('Connecting to controller at: {}'.format(args.ctrlr_ip))
+        for i in range(1, n_racs+1):
+            dpid=(i*16)+1
+            switches.append(self.addSwitch('s1r{}'.format(i), dpid='{}'.format(dpid)))
 
-    net=Mininet(topo=None, build=False)
+        return switches
 
-    h=[]
-    for i in range(args.hosts):
-        h.append(net.addHost('h{}'.format(str(i))))
+    def init_hosts(self, n_racs, n_hosts):
+        hosts=[]
+        for i in range(1, n_racs+1):
+            hosts.append([])
+            for j in range(1, n_hosts+1):
+                hosts[i-1].append(self.addHost('h{}r{}'.format(j, i)))
 
-    c0=net.addController('c0', controller=rc, ip=args.ctrlr_ip, port=6633)
+        return hosts
 
-    s=[]
-    for i in range(args.swi):
-        s.append(net.addSwitch('s{}'.format(str(i))))
+    def link_up(self, switches, hosts):
+        topo={}
 
-    print('****adding links****')
+        #extract root switch
+        root_sw=switches[0]
+        switches.remove(root_sw)
 
-    for swi in s:
-        num=1
-        if len(s)<len(h):
-            if swi==s[-1]:
-                #last switch, go all out
-                num=len(h)
-            else:
-                num=num+randombytes_uniform(len(h)-len(s))
+        #link up all hosts
+        i=0
+        for s in switches:
+            topo[s]=[]
+            for h in hosts[i]:
+                self.addLink(s, h)
+                topo[s].append(h)
+            i+=1
+            self.addLink(root_sw, s)
 
-        for i in range(num):
-            host=choice(h)
-            net.addLink(swi, host, )
-        swi.start([c0])
+        return topo
 
-    net.build()
-    cli(net)
-    net.stop()
+topos={
+        'dcenter':dcenterTopo
+        }
