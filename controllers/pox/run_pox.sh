d57b69a44ebd7b073710b4446909b445bd3343a9
r0ck3r <naman.arora@tutanota.com>
2019-02-14 01:24:25 +0530

    restarted relay in python
    Former-commit-id: 3fd851e6fc781b62a201f7267a03f5a0affcd363

--- controllers/pox/run_pox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-path='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/pox'
-
-docker run -e PATH=$path -v ${PWD}/ext:/pox/ext -v ${HOME}/git/sdn-proj/controllers/librelay:/pox/ext/librelay -it pox
--- relay/snd_rcv.h
+++
@@ -1,7 +0,0 @@
-#ifndef SND_RCV_H
-#define SND_RCV_H
-
-int snd(int, char *, char *, int);
-char *rcv(int, char *);
-
-#endif
