f44ce3549ab87c9e4ed2946b485ab032932554df
r0ck3r <naman.arora@tutanota.com>
2019-02-18 01:49:28 +0530

    added new main topology script with more topology building flexibility
    Former-commit-id: 3fb2ee871f9193232a3510dcd1f48c5a4e3764e1

--- mininet/topos/main.py
+++ mininet/topos/main.py
@@ -1,182 +1,215 @@
 from mininet.net import Mininet
-from mininet.log import setLogLevel
-from mininet.cli import CLI as cli
 from mininet.node import RemoteController as rc
-from argparse import ArgumentParser
-from MySQLdb import connect
+from mininet.cli import CLI as cli
+from mininet.log import setLogLevel
 from getpass import getpass
+from MySQLdb import connect
+from argparse import ArgumentParser
+from libnacl import randombytes_uniform as ru, sodium_init
 from sys import exit
 
-####DATABASE FUNCTIONS####
-def init_db(host, uname, passwd, db_name):
+#database
+def init_db(db_host, uname, passwd, db_name):
+    conn=None
     try:
-        conn=connect(host, user=uname, passwd=passwd, db=db_name)
-        print('[!]Connected to database {}'.format(db_name))
+        conn=connect(db_host, user=uname, passwd=passwd, db=db_name)
+        print('[!]Connected under the uname: {}'.format(uname))
 
         cur=conn.cursor()
+
         return (conn, cur)
     except Exception as e:
-        print('[-]Error in connecting to db {}: {}'.format(db_name, e))
-        exit(-1)
+        print('[-]Error in connecting under uname {}: {}'.format(uname, e))
+        if conn!=None:
+            conn.close()
 
 def send_query(t, query):
     try:
         t[1].execute(query)
-
-        if 'update' in query.lower() or 'insert' in query.lower():
-            t[0].commit()
-
-        print('[!]Query {} executed successfully'.format(query))
-
-        rows=t[1].fetchall()
-        ret=[]
-        for r in rows:
-            ret.append(i[0])
-
-        return ret
+        t[0].commit()
+        print('[!]Query executed Successfully')
     except Exception as e:
         print('[-]Error in executing query {}: {}'.format(query, e))
-        return False
 
-def update_db(t, topo, dump_mac):
-    ctrlr_ips=topo.keys()
-    for ip in ctrlr_ips:
-        #create table query
+
+def update_db(t, topo):
+    subnets=topo.values()
+    ctrlr_ip=topo.keys()
+    for i in range(len(ctrlr_ip)):
+        subnet=subnets[i]
+        ip=ctrlr_ip[i]
+        #create table
         query="CREATE TABLE `{}` (macs varchar(50));".format(ip)
-        if send_query(t, query)==False:
-            print('[-]Error in executing {}'.format(query))
-        
-        #insert query
-        query="INSERT INTO `{}` VALUES ('{}'), ".format(ip, dump_mac)
-        for h in topo[ip][1]:
-            query="".join([query, "('{}')".format(h.MAC())])
-            if h==topo[ip][1][-1]:
+        send_query(t, query)
+        #inset vals
+        query="INSERT INTO `{}` VALUES ('{}'), ".format(ctrlr_ip[i], subnet[0][0].MAC('s{}-eth{}'.format(i+1, len(subnets[1])+1)))
+        for h in subnet[1]:
+            query="".join([query, "('{}') ".format(h.MAC())])
+            if h==subnet[1][-1]:
                 query="".join([query, ";"])
             else:
-                query="".join([query, ", "])
-        if send_query(t, query)==False:
-            print('[-]Error in executing query {}'.format(query))
+                query="".join([query, " , "])
+        send_query(t, query)
 
-####DATABASE DUNCTIONS####
+#databse
 
-####TOPOLOGY FUNCTIONS####
+#mininet
 def parse_ctrlr_file(fname):
-    ctrlr_ips=[]
-    print('{}'.format(fname))
+    ctrlr_ip=[]
     with open(fname, 'r') as f:
-        ctrlr_ips=f.read().strip().split('\n')
+        ctrlr_ip=f.read().strip().split('\n')
 
-    print('[!]The controllers are detected at: {}'.format(ctrlr_ips))
-    return ctrlr_ips
+    print('[!]Registered controller IPs are: {}'.format(ctrlr_ip))
 
-def init_switches(net, subnets):
-    print('***Initiating switches***')
-    rel_sw=net.addSwitch('s{}'.format(subnets+1))
-    switches=[[rel_sw], []]
+    return ctrlr_ip
 
-    for i in range(1, subnets+1):
-        switches[1].append(net.addSwitch('s{}'.format(i)))
-
-    return switches
-
-def init_hosts(net, subnets, hosts_num):
-    print('***Initiating hosts***')
-    hosts=[]
-    k=1
-    for i in range(1, subnets+1):
-        hosts.append([])
-        for j in range(1, hosts_num+1):
-            hosts[i-1].append(net.addHost('h{}s{}'.format(j, i), ip='192.168.1.{}'.format(k)))
-            k+=1
-
-    return hosts
-
-def form_topo(net, ctrlr_ips, switches, hosts):
-    print('***Forming Links***')
+#topo is {"ctrlr ip": [["root_sw"], ["hosts"]]}
+def init_subnets(net, ctrlr_ip, n_subnets, n_hosts):
     topo={}
+    
+    for i in range(n_subnets):
+        topo[ctrlr_ip[i]]=[[], []]
+        sw=net.addSwitch('s{}'.format(i+1))
+        topo[ctrlr_ip[i]][0].append(sw)
 
-    for i in range(len(switches[1])):
-        sw=switches[1][i]
-        net.addLink(switches[0][0], sw)
-        topo[ctrlr_ips[i]]=[[sw], []]
-        for h in hosts[i]:
+        for j in range(1, n_hosts+1):
+            h=net.addHost('h{}s{}'.format(j, i+1))
             net.addLink(sw, h)
-            topo[ctrlr_ips[i]][1].append(h)
+            topo[ctrlr_ip[i]][1].append(h)
 
     return topo
 
-def init_ctrlrs(net, ctrlr_ips):
+def init_rel_sw(net, n_rel_sw, n_subnets):
+    rel_sw={}
+    k=0
+    for i in range(n_subnets+1, (n_subnets+n_rel_sw+1)):
+        sw=net.addSwitch('s{}'.format(i))
+        rel_sw[sw]=2
+        #the unlucky ones given stright line connectivity(terminal rel_sw)
+        if k!=2:
+            rel_sw[sw]=rel_sw[sw]-1
+            k+=1
+
+    rels=rel_sw.keys()
+    n=len(rels)
+    for i in range(n):
+        sw=rels[i]
+        rels.remove(sw)
+        links=[]
+        k=rel_sw[sw]
+        if len(rels)==0:
+            break
+        for j in range(k):
+            s=rels[ru(len(rels))]
+            if rel_sw[s]!=0 and s not in links:
+                net.addLink(sw, s)
+                links.append(s)
+                rel_sw[s]=rel_sw[s]-1
+                rel_sw[sw]=rel_sw[sw]-1
+
+    return rel_sw.keys()
+
+def choice(t):
+    ret=t[ru(len(t))]
+    t.remove(ret)
+    return ret
+
+def assign_rel_sw(net, rel_sw, topo):
+    subnets=topo.values()
+    n=len(rel_sw)
+    for i in range(len(rel_sw)):
+        rel=choice(rel_sw)
+        num=1
+        if len(subnets)>len(rel_sw):
+            if len(rel_sw)==0:
+                #go all out
+                num=len(subnets)
+            else:
+                #give random number of subnets
+                num=num+ru(len(subnets)-len(rels))
+
+        for j in range(num):
+            subnet=choice(subnets)
+            net.addLink(rel, subnet[0][0])
+
+
+def init_ctrlrs(net, ctrlr_ip):
     ctrlrs=[]
-    for i in range(1, len(ctrlr_ips)+1):
-        ctrlrs.append(net.addController('c{}'.format(i), controller=rc, ip=ctrlr_ips[i-1], port=6633))
+    for i in range(1, len(ctrlr_ip)+1):
+        ctrlrs.append(net.addController('c{}'.format(i), controller=rc, ip=ctrlr_ip[i-1], port=6633))
 
     return ctrlrs
 
-def form_ctrlr_links(ctrlrs, switches):
-    print('***Starting switches with ctrlrs***')
-    for i in range(len(switches[1])):
-        switches[1][i].start([ctrlrs[i]])
+def init_switches(ctrlrs, topo, rel_sw):
+    ctrlr_ip=topo.keys()
+    j=0
+    for i in range(len(ctrlrs)):
+        ctrlr=ctrlrs[i]
+        if ctrlr.IP() in ctrlr_ip:
+            topo[ctrlr.IP()][0][0].start([ctrlr])
+        else:
+            rel_sw[j].start([ctrlr])
+            j+=1
+#Mininet
 
-    switches[0][0].start([ctrlrs[-1]])
-####TOPOLOGY FUNCTIONS####
-
-####MAIN CALLEE####
 if __name__=='__main__':
     parser=ArgumentParser()
-    parser.add_argument('-c', '--ctrlr_file', required=True, dest='ctrlr_file', metavar='', help="The file containing the controllers' IP")
-    parser.add_argument('-d' '--db_host', required=True, dest='db_host', metavar='', help="The db image host IP")
-    parser.add_argument('-u', '--uname', dest='uname', metavar='', help="The user name to login to the database")
-    parser.add_argument('-s', '--subnets', dest='subnets', required=True, type=int, metavar='', help="The number of subnets")
-    parser.add_argument('-H', '--hosts', dest='hosts', required=True, type=int, metavar='', help="The number of hosts per subnet")
+    parser.add_argument('-c', '--ctrlr_file', required=True, dest='ctrlr_file', metavar='', help='The file with IP addresses of all the controllers')
+    parser.add_argument('-d', '--db_host', required=True, dest='db_host', metavar='', help='The database host name')
+    parser.add_argument('-u', '--uname', dest='uname', metavar='', help="The username under which database connection is made (Default 'topology')")
+    parser.add_argument('-s', '--subnets', required=True, type=int, metavar='', dest='subnets', help='The number of subnets in the network')
+    parser.add_argument('-r', '--rel_sw', required=True, type=int, metavar='', dest='rel_sw', help='The number of relay switches')
+    parser.add_argument('-H', '--hosts', required=True, type=int, metavar='', dest='hosts', help='The number of hosts per subnet')
     args=parser.parse_args()
 
-    #parse ctrlr file
-    ctrlr_ips=parse_ctrlr_file(args.ctrlr_file)
+    #log
+    setLogLevel('info')
 
-    #check sanity
-    if len(ctrlr_ips)!=(args.subnets+1):
-        print('[-]Error subnets number and controllers dont match.')
+    #ctrlr_ip file
+    ctrlr_ip=parse_ctrlr_file(args.ctrlr_file)
+
+    #sanity ch
+    if len(ctrlr_ip)!=(args.subnets+args.rel_sw):
+        print('[-]Not enough controllers available. Exiting...')
         exit(-1)
 
+    #init libnacl
+    sodium_init()
+
     #mininet
-    setLogLevel('info')
     net=Mininet(topo=None, autoSetMacs=True)
 
-    #form switches
-    switches=init_switches(net, args.subnets)
+    #db init
+    passwd=getpass('Enter the password for username {}: '.format(('topology' if args.uname==None else args.uname)))
+    conn_net, cur_net=init_db(args.db_host, ('topology' if args.uname==None else args.uname), passwd, 'network')
 
-    #form hosts
-    hosts=init_hosts(net, args.subnets, args.hosts)
+    #form subnets
+    topo=init_subnets(net, ctrlr_ip, args.subnets, args.hosts)
 
-    #form topology dict
-    topo=form_topo(net, ctrlr_ips, switches, hosts)
+    #form relay switches
+    rel_sw=init_rel_sw(net, args.rel_sw, args.subnets)
 
-    #calc dump mac
-    dump_mac=switches[0][0].MAC('s{}-eth1'.format(args.subnets+1))
+    #assign subnets to relay switches
+    assign_rel_sw(net, [sw for sw in rel_sw], topo)
+
+    #build topology
+    net.build()
 
     #update db
-    passwd=getpass('Enter the password for username {}: '.format('topology' if args.uname==None else args.uname))
-    conn_net, cur_net=init_db(args.db_host, ('topology' if args.uname==None else args.uname), passwd, 'network')
-    update_db((conn_net, cur_net), topo, dump_mac)
-
-    #instructional wait
-    print("[!]Networks Databse has been updated, please throughly check for each controller, start every controller and THEN type 'BUILD' below to build the mininet topology...")
-    while True:
-        inst=str(raw_input('[>] '))
-        if inst=='BUILD' or inst=='build':
-            break
-        else:
-            print('[-]Invalid instruction!!')
+    update_db((conn_net, cur_net), topo)
 
     #init controllers
-    ctrlrs=init_ctrlrs(net, ctrlr_ips)
+    ctrlrs=init_ctrlrs(net, ctrlr_ip)
 
-    #form links
-    form_ctrlr_links(ctrlrs, switches)
+    #init switches
+    init_switches(ctrlrs, topo, rel_sw)
 
-    #necessary shananigans
-    net.build()
+    #init cli
     cli(net)
+
+    #stop
     net.stop()
 
-####MAIN CALEE####
+    #end connection with db
+    cur_net.close()
+    conn_net.close()
