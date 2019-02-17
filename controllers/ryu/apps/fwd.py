ad290ef1a1ceb987c5e759a97d3c7e415f970c6a
r0ck3r <naman.arora@tutanota.com>
2019-02-18 01:56:11 +0530

    generic 12 learning switch with database support
    Former-commit-id: 89a9ef809c73dbc5341471816cb019e3d8987ccf

--- controllers/ryu/apps/fwd.py
+++ controllers/ryu/apps/fwd.py
@@ -1,117 +1,83 @@
 from ryu.base import app_manager
-from ryu.ofproto import ofproto_v1_2
 from ryu.controller import ofp_event
-from ryu.controller.handler import set_ev_cls
 from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_2
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet, arp
+from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from importlib import import_module
 
-db=['192.168.1.2', '192.168.1.3']
+ctrlr_handler=import_module('ctrlr_handler', '/ryu/apps')
 
-#every class needs to inherit app_manager RyuApp class
-class learn_sw(app_manager.RyuApp):
-    #define ofp version
-    OFP_VERSIONS=[ofproto_v1_2.OFP_VERSION]
+class SimpleSwitch12(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_2.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(learn_sw, self).__init__(*args, **kwargs)
-        self.mac_to_port={}
+        super(SimpleSwitch12, self).__init__(*args, **kwargs)
+        self.macs=ctrlr_handler.init()
+        self.mac_to_port = {}
+        print('The MAC addresses in network are: {}'.format(self.macs))
 
     def add_flow(self, datapath, port, dst, src, actions):
-        #extract openflow protocol
-        ofproto=datapath.ofproto
+        ofproto = datapath.ofproto
 
-        #create a match entry for the new flow
-        match=datapath.ofproto_parser.OFPMatch(
-                in_port=port,
-                eth_dst=dst,
-                eth_src=src)
+        match = datapath.ofproto_parser.OFPMatch(in_port=port,
+                                                 eth_dst=dst,
+                                                 eth_src=src)
+        inst = [datapath.ofproto_parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS, actions)]
 
-        #make the instruction list that must be followed if match takes place
-        inst=[datapath.ofproto_parser.OFPInstructionActions(
-            ofproto.OFPIT_APPLY_ACTIONS, actions)]
-
-        mod=datapath.ofproto_parser.OFPFlowMod(
-                datapath=datapath, cookie=0, cookie_mask=0, table_id=0,
-                command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-                priority=0, buffer_id=ofproto.OFP_NO_BUFFER, out_port=ofproto.OFPP_ANY,
-                out_group=ofproto.OFPG_ANY,
-                flags=0, match=match, instructions=inst)
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, cookie=0, cookie_mask=0, table_id=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=0, buffer_id=ofproto.OFP_NO_BUFFER,
+            out_port=ofproto.OFPP_ANY,
+            out_group=ofproto.OFPG_ANY,
+            flags=0, match=match, instructions=inst)
         datapath.send_msg(mod)
 
-
-    #decorator function decorated pkt_fwd function
-    #has EventOFPPacketIn as first argument 
-    #has second argument main_dispatcher as main handeller
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    #event as argument
-    def pkt_fwd(self, ev):
-        #print('Calling pkt_fwd in learning_switch')
-        #extract msg from the event
-        msg=ev.msg
-        #extract datapath, which entails basic info about the path of data flow
-        #like ports macs etc
-        datapath=msg.datapath
-        #extract openflow protocol version
-        ofproto=datapath.ofproto
-        #extract in port
-        in_port=msg.match['in_port']
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        in_port = msg.match['in_port']
 
-        #msg.data has the raw data the packet has to offer
-        #packet.Packet function extracts the data to pkt datatype
-        pkt=packet.Packet(msg.data)
-        #get the very first protocol or the physical layer header
-        eth=pkt.get_protocols(ethernet.ethernet)[0]
-        arp_hdr=pkt.get_protocols(arp.arp)[0]
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        if arp!=[]:
-            print(arp_hdr)
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
             return
-            
-        else:
-            #ignore lldp packet as it doesnt have dst and source
-            if eth.ethertype==ether_types.ETH_TYPE_LLDP:
-                return
+        dst = eth.dst
+        src = eth.src
 
-            #extract source and destination
-            src=eth.src
-            dst=eth.dst
-
-
-        #extract datapath id
-        dpid=datapath.id
-        #set default value of dpid in mat_to_port dict equal to null dict
+        dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        #learn thr current src and dpid to avoid flood next time
-        #this dictionary maps datapathid to perticular src mac and in_port
-        self.mac_to_port[dpid][src]=in_port
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        #assign outport equal to mac_to_port value corresponding same dpid and dst
-        #if previously assigned, here if current src was to become a dst another
-        #time, current in_port will be used as out_port that time, making them 
-        #cling to each forever, other wise flood every possible port
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
         if dst in self.mac_to_port[dpid]:
-            out_port=self.mac_to_port[dpid][dst]
-        else:
-            out_port=ofproto.OFPP_FLOOD
+            out_port = self.mac_to_port[dpid][dst]
+            #check here if contents of this match the self.mac, if yes, means arp is done, send to relay
+        if dst in self.macs:
+            out_port = ofproto.OFPP_FLOOD
 
-        #assign actions list using OFPActionOutput
-        actions=[datapath.ofproto_parser.OFPActionOutput(out_port)]
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
 
-        #install a new flow to avoid packet_in function next time, provided pkt
-        #is not flooded
-        if out_port!=ofproto.OFPP_FLOOD:
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
             self.add_flow(datapath, in_port, dst, src, actions)
 
-        data=None
-        #check if the buffer is null, if yes assign msg.data to data
-        if msg.buffer_id==ofproto.OFP_NO_BUFFER:
-            data=msg.data
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
 
-        out=datapath.ofproto_parser.OFPPacketOut(
-                datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
-                actions=actions, data=data)
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
+            actions=actions, data=data)
         datapath.send_msg(out)
-
