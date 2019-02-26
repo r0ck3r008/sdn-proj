f851da2e6c2e40a05cf0f34f7377888aeb11ae5b
r0ck3r <naman.arora@tutanota.com>
2019-02-27 02:13:21 +0530

    initial code of redesigned relay
    Former-commit-id: e9d0f19f220a9a372908e860894c53a8a0e218da

---
+++ relay/cli_run.py
@@ -0,0 +1,88 @@
+from sys import exit, stderr
+from importlib import import_module
+from threading import Thread as thread
+
+db_workings=import_module('db_workings', '.')
+svr_workings=import_module('svr_workings', '.')
+
+def snd(sock, msg, addr):
+    try:
+        sock.send(msg.encode())
+    except Exception as e:
+        print('[-]Error in sending {} to {}: {}'.format(msg, addr[0], e), file=stderr)
+
+def rcv(sock, addr):
+    try:
+        msg=sock.recv(2048).decode()
+        return msg
+    except Exception as e:
+        print('[!]Error in receving from {}: {}'.format((addr[0] if addr!=None else 'UDS client'), e), file=stderr)
+
+def handle_ctrlr_p1(c_sock, addr, db_info, uds_addr):
+    #get db connection
+    conn, cur=db_workings.init_cxn(db_info[0], db_info[1], db_info[2], db_info[3])
+
+    #send initial host list
+    tables=db_workings.send_query((conn, cur), 'SELECT macs FROM `{}`'.format(addr[0]))
+    msg=''
+    for t in tables:
+        msg="".join([msg, t])
+    snd(c_sock, msg, addr)
+
+    #create UDS client socket
+    uds_sock=svr_workings.sock_create(uds_addr, 2)
+
+    while True:
+        cmdr=rcv(c_sock, addr)
+        if 'exit' in cmd.lower():
+            break
+        snd(uds_sock, cmdr)
+
+        #TODO
+        #update db        
+
+    #end stuff
+    print('[!]Closing connection to {}'.format(addr[0]))
+    conn.close()
+    cur.close()
+    c_sock.close()
+    uds_close()
+
+def handle_ctrlr_p2(addr, pipe_name):
+    #get a connection back to controller
+    sock=svr_workings.sock_create(addr, 1)
+
+    #open the fifo
+    while True:
+        with open(pipe_name, 'r') as pipe:
+        #listen for fifo updates and keep updating the controller
+            cmdr=pipe.read()
+            snd(sock, cmdr)
+
+    print('[!]Closing downlink connection with {}'.format(addr[0]))
+    sock.close()
+
+#There are two threads for each client
+#the first process handles the inbound blacklist and whitelist requests from the controller and sends the same to the UDS server within relay and updates in the db
+#the second process listens for the intercontroller blacklist and whitelist requests sent by the UDS server on the fifo 
+
+def handle_ctrlr(c_sock, addr, db_info, uds_addr, pipe_name):
+    print('[!]Handelling client {}'.format(addr[0]))
+
+    #create first thread
+    thr_p1=thread(target=handle_ctrlr_p1, args=(c_sock, addr, db_info))
+    thr_p1.start()
+
+    #create second thread
+    thr_p2=thread(target=handle_ctrlr_p2, args=(addr, pipe_name))
+    thr_p2.start()
+
+    #join them
+    thr_p1.join()
+    thr_p2.join()
+
+def handle_uds(c_sock, pipe_name):
+    while True:
+        cmdr=rcv(c_sock, None)
+        with open(pipe_name, 'w') as pipe:
+            pipe.write(cmdr)
---
+++ relay/svr_workings.py
@@ -0,0 +1,67 @@
+from socket import socket, AF_INET, SOCK_STREAM, AF_UNIX
+from sys import exiti, stderr
+from multiprocessing import Process as process
+from importlib import import_module
+from os import path
+
+db_workings=import_module('db_workings', '.')
+cli_run=import_module('cli_run', '.')
+
+def sock_create(addr, flag):
+    sock=None
+    try:
+        if flag==0:
+            sock=socket(AF_INET, SOCK_STREAM)
+            sock.bind(addr)
+            sock.listen(5)
+            print('[!]Server socket bound and listening...')
+        elif flag==1:
+            sock=socket(AF_INET, SOCK_STREAM)
+            sock.connect(addr)
+            print('[!]Connection to {} success'.format(addr[0]))
+        elif flag==2:
+            sock=socket(AF_UNIX, SOCK_STREAM)
+            if path.exists(addr):
+                sock.connect(addr)
+                print('[!]Connected to UDS as client..')
+            else:
+                sock.bind(addr)
+                sock.listen(5)
+                print('[!]UDS server listening for connections...')
+        return sock
+    except Exception as e:
+        print('[-]Error in creating socket server: {}'.format(e), file=stderr)
+        if sock!=None:
+            sock.close()
+            exit(-1)
+
+def svr_loop(svr_sock, db_info, uds_addr, pipe_name):
+    i=-1
+    while True:
+        i+=1
+        sock=None
+        try:
+            sock, addr=svr_sock.accept()
+            print('[!]Successful connection to {}.'.format(addr[0]))
+            proc=process(target=cli_run.handle_ctrlr, args=(sock, addr, db_info, uds_addr, pipe_name))
+            proc.start()
+            print('[!]{} handle process started successfully'.format(addr[0]))
+        except exception as e:
+            print('[-]Error in handelling client {}'.format(i), file=stderr)
+            if sock!=None:
+                sock.close()
+
+def uds_loop(uds_sock, uds_addr, pipe_name):
+    i=0
+    while True:
+        sock=None
+        try:
+            sock, addr=uds_sock.accept()
+            print('[!]New UDS connection!')
+            proc=process(target=cli_run.handle_uds, args=(sock, pipe_name))
+            proc.start()
+            i+=1
+        except Exception as e:
+            print('[-]Error in starting UDS client handler {}: {}'.format(i, e))
+            if sock!=None:
+                sock.close()
