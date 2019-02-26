45002996d69dec273239aff97492e27ea83905b9
r0ck3r <naman.arora@tutanota.com>
2019-02-27 03:52:24 +0530

    integrated the threading functionality with svr and cli threads which connect with downlink and uplink of relay
    Former-commit-id: 2a0d670002aff204abce19b6e57298fc9dedf23f

--- controllers/ryu/apps/rootsw_ctrlr/fwd.py
+++ controllers/ryu/apps/rootsw_ctrlr/fwd.py
@@ -6,14 +6,31 @@ from ryu.ofproto import ofproto_v1_2
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from threading import Thread as thread, Lock as lock
+from importlib import import_module
+
+helper=import_module('helper', '/ryu/apps/rootsw_ctrlr')
+cfg=import_module('cfg', '/ryu/apps/rootsw_ctrlr')
 
 class SimpleSwitch12(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_2.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch12, self).__init__(*args, **kwargs)
+        #global definitions
         self.mac_to_port = {}
         self.blacklist=[]
+        rel_addr=(cfg.rel_addr, cfg.rel_port)
+        self_addr=(cfg.self_addr, (12346 if cfg.port==None else cfg.port))
+        pipe_name=('pipe' if cfg.pipe_name==None else cfg.pipe_name)
+        self.mtx=lock()
+        #start server thread
+        svr_thr=thread(target=helper.svr_run, args=(self_addr, self.blacklist, self.mtx))
+        svr_thr.start()
+
+        #start client thread
+        cli_thr=thread(target=helper.cli_run, args=(rel_addr, pipe_name))
+        cli_thr.start()
 
     def add_flow(self, datapath, port, dst, src, actions):
         ofproto = datapath.ofproto
@@ -21,12 +38,14 @@ class SimpleSwitch12(app_manager.RyuApp):
         match = datapath.ofproto_parser.OFPMatch(in_port=port,
                                                  eth_dst=dst,
                                                  eth_src=src)
+        self.mtx.acquire()
         if dst not in self.blacklist:
             inst = [datapath.ofproto_parser.OFPInstructionActions(
                     ofproto.OFPIT_APPLY_ACTIONS, actions)]
         else:
             inst = [datapath.ofproto_parser.OFPInstructionActions(
                     ofproto.OFPIT_CLEAR_ACTIONS, [])]
+        self.mtx.release()
 
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, cookie=0, cookie_mask=0, table_id=0,
@@ -61,6 +80,7 @@ class SimpleSwitch12(app_manager.RyuApp):
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
+        self.mtx.acquire()
         if dst in self.blacklist:
             out_port=ofproto.OFPP_IN_PORT
         elif dst in self.mac_to_port[dpid]:
@@ -68,6 +88,7 @@ class SimpleSwitch12(app_manager.RyuApp):
             #check here if contents of this match the self.mac, if yes, means arp is done, send to relay
         else:
             out_port = ofproto.OFPP_FLOOD
+        self.mtx.release()
 
         actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
 
