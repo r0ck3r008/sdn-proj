b1486079f3d643d3c71f025ee25798618434d4b1
r0ck3r008 <naman.arora@tutanota.com>
2019-03-27 17:03:42 +0530

    added a suprelay_file option to the argument parser which passes it on to the ids_workings.ids_svr_loop
    Former-commit-id: f8a6b90f4e5fdc4d08f7733307914bd5bc543592

--- relay/main.py
+++ relay/main.py
@@ -11,6 +11,7 @@ def init_glbls(args):
     glbls['bind_addr']=args.bind_addr
     glbls['bind_port']=args.bind_port
     glbls['uds_sock_name']='./uds' if args.uds_sock_name==None else args.uds_sock_name
+    glbls['suprelay_file']=args.suprelay_file
 
     return glbls
 
@@ -20,6 +21,7 @@ if __name__=='__main__':
     parser.add_argument('-b', '--bind_addr', required=True, metavar='', dest='bind_addr', help='The address to bind for relay')
     parser.add_argument('-p', '--bind_port', required=True, metavar='', type=int, dest='bind_port', help='The port to bind for relay')
     parser.add_argument('-uS', '--uds_sock_name', metavar='', dest='uds_sock_name', help="The URI of the UDS socket (Default is './uds')")
+    parser.add_argument('-sR', '--suprelay_file', metavar='', dest='suprelay_file', help="Path of a file containing the address of super-relay(s)")
     args=parser.parse_args()
     glbls=init_glbls(args)
 
@@ -31,7 +33,7 @@ if __name__=='__main__':
     uds_svr_proc=process(target=uds_workings.uds_svr_loop, args=[uds_sock, ids_sock])
     uds_svr_proc.start()
 
-    ids_svr_proc=process(target=ids_workings.ids_svr_loop, args=[ids_sock, uds_sock, glbls['uds_sock_name']])
+    ids_svr_proc=process(target=ids_workings.ids_svr_loop, args=[ids_sock, uds_sock, glbls['uds_sock_name'], glbls['suprelay_file']])
     ids_svr_proc.start()
 
     #join
